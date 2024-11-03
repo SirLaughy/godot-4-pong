8b8e458878c473c57ca30923d7b24cd49c4a3b2f
SirLaughy <tan.hole@hotmail.com>
2024-11-03 16:31:59 +0800

    Created Base Controls Scene

--- Scripts/Globals/GlobalConfig.gd
+++ Scripts/Globals/GlobalConfig.gd
@@ -2,55 +2,63 @@ extends Node
 
 class_name Global_Config
 
-const SAVEFILE = "user://nimbusballconfig.save"
+const SAVEFILE 						= "user://nimbusballconfig.save"
 
-var sfx_volume : float
-var music_volume : float
-var screen_shake_level : float
-var screen_size : Vector2
-var controls = {
-	
-}
+
+var sfx_volume 						: float
+var music_volume 					: float
+var screen_shake_level 				: float
+var screen_size 					: Vector2
+var controls 						= {}
+var configs 						= {}
 
 # TODO: Keybind Mapping
 # TODO: Audio Channel
 
 func _ready():
+	configs = {
+		"sfx_volume" 				: sfx_volume 			as float,
+		"music_volume" 				: music_volume 			as float,
+		"screen_shake_level" 		: screen_shake_level 	as float,
+		"controls" 					: controls 				as Dictionary
+	}
 	load_config()
 
 func load_config():
 	if FileAccess.file_exists(SAVEFILE):
-		var file = FileAccess.open(SAVEFILE, FileAccess.READ)
-		sfx_volume = file.get_var(sfx_volume)
-		music_volume = file.get_var(music_volume)
-		screen_shake_level = file.get_var(screen_shake_level)
-		controls = file.get_var(controls)
+		var file 					= FileAccess.open(SAVEFILE, FileAccess.READ)
+		var loaded_configs 			= file.get_var(configs)
+		
+		configs.sfx_volume 			= loaded_configs.sfx_volume
+		configs.music_volume 		= loaded_configs.music_volume
+		configs.screen_shake_level 	= loaded_configs.screen_shake_level
+		configs.controls 			= loaded_configs.controls
+		
+		file = null
 	else:
-		sfx_volume = 1.0
-		music_volume = 1.0
-		screen_shake_level = 1.0
-		controls = {
-			"player1_up" 	: KEY_W,
-			"player1_down" 	: KEY_S,
-			"player1_pause" : KEY_ESCAPE,
-			"player2_up" 	: KEY_UP,
-			"player2_down" 	: KEY_DOWN,
-			"player2_pause" : KEY_ENTER
+		configs.sfx_volume 			= 1.0
+		configs.music_volume 		= 1.0
+		configs.screen_shake_level 	= 1.0
+		configs.controls 			= {
+			"player1_up" 			: KEY_W,
+			"player1_down" 			: KEY_S,
+			"player1_pause" 		: KEY_ESCAPE,
+			"player2_up" 			: KEY_UP,
+			"player2_down" 			: KEY_DOWN,
+			"player2_pause" 		: KEY_ENTER
 		}
 	bind_controls()
 	
 func save_config():
-	var file = FileAccess.open(SAVEFILE, FileAccess.WRITE)
-	file.store_var(sfx_volume)
-	file.store_var(music_volume)
-	file.store_var(screen_shake_level)
-	file.store_var(controls)
-	file = null
+	var file 						= FileAccess.open(SAVEFILE, FileAccess.WRITE)
+	file.store_var(configs)
+	file 							= null
 
 func bind_controls() -> void:
 	var key_event
 	for control in controls:
+		key_event 					= InputEventKey.new()
+		key_event.keycode 			= controls[control]
+		
 		InputMap.action_erase_events(control)
-		key_event = InputEventKey.new()
-		key_event.keycode = controls[control]
 		InputMap.action_add_event(control, key_event)
